003a611f0a072d9a4cc682e67ef8fe9695d2375e
Ksenia <aliksukerch@gmail.com>
2023-08-06 15:57:36 +0200

    redeployed

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/goit-react-hw-06-phonebook">
-    <React.StrictMode>
+    <StrictMode>
      <App />
-    </React.StrictMode>
+    </StrictMode>
   </BrowserRouter>
 );
